7552750d719811de7d7b01884d819b36d77d32f0
franciscovargas <vargfran@gmail.com>
2018-11-24 17:33:26 +0000

    bad plots

--- src/create_plots.py
+++ src/create_plots.py
@@ -6,31 +6,29 @@ import matplotlib.pyplot as plt
 from datetime import datetime
 from dateutil import parser
 
-from os import path, listdir, curdir
+from data_paths import bad_plots, good_plots, listdir, path
 
 
-notebook_path = path.realpath(__file__)
-data_path = path.join(path.dirname(path.dirname(notebook_path)), "data")
-audio_path = path.join(data_path, "Sample Audio")
-good_plots = path.join(data_path, "good_examples")
+def plot_signals(wav_dir):
+    fss = []
+    datas = []
+    files  =[]
 
-fss = []
-datas = []
-files  =[]
+    for file_ in listdir(wav_dir):
+       fs,data = wavfile.read(path.join(wav_dir, file_))
+       fss.append(fs)
+       _filename = str(parser.parse(file_.split("_")[-1].split(".")[0]))
+       files.append(_filename.replace(":", "_"))
+       datas.append(data)
 
-for file_ in listdir(good_plots):
-   fs,data = wavfile.read(path.join(good_plots, file_))
-   fss.append(fs)
-   _filename = str(parser.parse(file_.split("_")[-1].split(".")[0]))
-   files.append(_filename.replace(":", "_"))
-   # import pdb; pdb.set_trace()
-   datas.append(data)
+    for i in range(len(datas)):
+       print(i)
+       plt.figure()
+       plt.plot(datas[i])
+       plt.savefig(files[i])
+       plt.clf()
 
-# datas = datas[]
-for i in range(len(datas)):
-   print(i)
-   # if i < 8: continue
-   plt.figure()
-   plt.plot(datas[i])
-   plt.savefig(files[i])
-   plt.clf()
+
+if __name__ == '__main__':
+    plot_signals(bad_plots)
+    plot_signals(good_plots)
---
+++ src/data_paths.py
@@ -0,0 +1,8 @@
+from os import path, listdir, curdir
+
+
+notebook_path = path.realpath(__file__)
+data_path = path.join(path.dirname(path.dirname(notebook_path)), "data")
+audio_path = path.join(data_path, "Sample Audio")
+good_plots = path.join(data_path, "good_examples")
+bad_plots = path.join(data_path, "failure_examples")
